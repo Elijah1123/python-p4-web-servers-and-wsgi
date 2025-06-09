3491ae930d340fa4488aafda43c7c2182bbc8436
Elijah M <elijah.macharia@student.moringaschool.com>
2025-06-09 18:41:38 +0300

    web-sever

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f'This web sever is running at {request.host}')
+    return Response('A WSGI generated thi response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+         hostname= 'localhost',
+         port= 5555,
+        application=application,
+    )
+    # run_simple('

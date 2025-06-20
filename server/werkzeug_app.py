3ad08b24e37fd816be4237c4ee5e51101db50b5e
Elijah M <elijah.macharia@student.moringaschool.com>
2025-06-20 11:58:07 +0300

    web-server

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
 from werkzeug.wrappers import Request, Response
 
 @Request.application
 def application(request):
-    print(f'This web sever is running at {request.host}')
-    return Response('A WSGI generated thi response!')
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response !')
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(
-         hostname= 'localhost',
-         port= 5555,
-        application=application,
+        hostname='localhost',
+        port=5555,
+        application=application
+
     )
-    # run_simple('
+ 
